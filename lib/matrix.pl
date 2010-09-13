7c06b79e8198e4cb4ecd54f88f14d9952d4f46ce
Aki Mimoto <aki-github@bako.ca>
2010-09-12 21:06:07 -0700

    Can now turn on and off leds! :)

--- lib/matrix.pl
+++ lib/matrix.pl
@@ -1,25 +1,36 @@
 #!/usr/bin/perl
 
 use strict;
-use vars qw/ $PULSE_LEN /;
+use vars qw/ $PULSE_LEN $PIN_LOOKUP /;
 use Data::Dumper;
 use Firmata::Arduino::Tied::Constants qw/ :all /;
 use Firmata::Arduino::Tied::Protocol;
 use Firmata::Arduino::Tied;
+
+use constant ($PIN_LOOKUP={
+    CS1  => 5,
+    CS2  => 6,
+    CS3  => 7,
+    CS4  => 8,
+
+    WR   => 2,
+    RD   => 3,
+    DATA => 4,
+});
+
+#$Firmata::Arduino::Tied::DEBUG = 1;
+$PULSE_LEN = 0.001;
+
 $|++;
 
 my $device = Firmata::Arduino::Tied->open('/dev/ttyUSB0');
 
-$device->probe;
-
-$PULSE_LEN = 0.1;
-
 # Pin connection table
 
-# cs1  - ground
-# cs2  - ground
-# cs3  - ground
-# cs4  - ground
+# cs1  - digital 5
+# cs2  - digital 6
+# cs3  - digital 7
+# cs4  - digital 8
 # osc  - ground
 # sync - floating
 
@@ -27,48 +38,104 @@ $PULSE_LEN = 0.1;
 # rd   - clock read signal - digital 3
 # data - 1 bit data.       - digital 4
 
-$device->pin_mode(2=>PIN_OUTPUT);
-$device->pin_mode(3=>PIN_OUTPUT);
-$device->pin_mode(4=>PIN_OUTPUT);
+# Create all the functions that we'll use to play with the pins
+no strict 'refs';
+while ( my ($name,$pin) = each %$PIN_LOOKUP ) {
+    $device->pin_mode($pin=>,PIN_OUTPUT);
+    my $lc_name = lc $name;
+    my $lc_sub = *{"::".$lc_name} = sub { 
+        print "$name is $_[0]\n";
+        $device->digital_write($pin=>$_[0]);
+        select undef, undef, undef, $PULSE_LEN;
+    };
+    *{"::".$lc_name."_pulse"} = sub { 
+        print "Pulsing: $name\n";
+        $device->digital_write($pin=>0);
+        select undef, undef, undef, $PULSE_LEN;
+        $device->digital_write($pin=>1);
+        select undef, undef, undef, $PULSE_LEN;
+    };
+    *{"::".$lc_name."_pulse_inv"} = sub { 
+        print "Pulsing: $name\n";
+        $device->digital_write($pin=>1);
+        select undef, undef, undef, $PULSE_LEN;
+        $device->digital_write($pin=>0);
+        select undef, undef, undef, $PULSE_LEN;
+    };
 
+}
+use strict;
+
+# Now let's initialize firmata
+$device->probe;
+
+# Set all pins high since that seems to be the default state
+cs1(1);
 wr(1);
 rd(1);
 data(1);
 
-my $iteration = 0;
+# Disable the unit
+cs1(0);
+preamble_send($device,"100");
+data_send($device,"000000000");
 
-#$Firmata::Arduino::Tied::DEBUG = 1;
+# Turn on the LEDs
+cs1_pulse_inv();
+preamble_send($device,"100");
+data_send($device,"000000111");
+
+# Turn on system oscillator
+cs1_pulse_inv();
+preamble_send($device,"100");
+data_send($device,"000000011");
+
+# Commons option
+cs1_pulse_inv();
+preamble_send($device,"100");
+data_send($device,"001010111");
 
 while (1) {
-    $device->poll;
-    matrix_write( $device, 0x10, 0x0f );
-    select undef,undef,undef,0.01;
+    # Wipe the screen
+    cs1_pulse_inv();
+    preamble_send($device,"101");
+    data_send($device,"0000000"); # address (MA)
+    for (1..64) {
+        data_send($device,"0000"); # data (MA)
+    }
+
+    # Turn on the screen
+    cs1_pulse_inv();
+    preamble_send($device,"101");
+    data_send($device,"0000000"); # address (MA)
+    for (1..64) {
+        data_send($device,"1111"); # data (MA)
+    }
 }
 
-sub wr {
+
+cs1(1);
+
+
+sub preamble_send {
 # --------------------------------------------------
-    print "WR: ".($_[0])."\n";
-    $device->digital_write(2=>$_[0]);
+    my ( $device, $data ) = @_;
+
+    my $buf = substr $data, 0, 3;
+
+    for my $d (split //, $buf) {
+        data($d);
+        wr_pulse();
+    }
 }
 
-sub wr_pulse {
+sub data_send {
 # --------------------------------------------------
-    select undef, undef, undef, $PULSE_LEN;
-    wr(0);
-    select undef, undef, undef, $PULSE_LEN;
-    wr(1);
-}
-
-sub rd {
-# --------------------------------------------------
-    print "RD: ".($_[0])."\n";
-    $device->digital_write(3=>$_[0]);
-}
-
-sub data {
-# --------------------------------------------------
-    print "DATA: ".($_[0])."\n";
-    $device->digital_write(4=>$_[0]);
+    my ( $device, $data ) = @_;
+    for my $d (split //, $data) {
+        data($d);
+        wr_pulse();
+    }
 }
 
 sub matrix_write {
@@ -76,12 +143,7 @@ sub matrix_write {
     my ( $device, $address, $data ) = @_;
 
 # 3 preamble bits 101
-    data(1);
-    wr_pulse();
-    data(0);
-    wr_pulse();
-    data(1);
-    wr_pulse();
+    preamble(qw( 1 0 1 ));
 
 # Then the address, which is 7 bits
     for my $i ( 0..6 ) {
