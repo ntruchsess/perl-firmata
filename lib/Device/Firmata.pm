08b38f12ba7ccc9142097bf60a7466c7c3ed582b
Jens B. <jnsbyr@users.noreply.github.com>
2026-01-05 22:00:32 +0100

    CPAN release "0.70.1" (#34)
    - fixed Device::Firmata::Base::import by removing call to SUPER::import

--- lib/Device/Firmata.pm
+++ lib/Device/Firmata.pm
@@ -19,11 +19,11 @@ This module allows a computer running Perl to connect to Firmata devices (Arduin
 
 =head1 VERSION
 
-Version 0.69
+Version 0.70
 
 =cut
 
-our $VERSION = '0.69';
+our $VERSION = '0.70';
 our $DEBUG = 0;
 
 
